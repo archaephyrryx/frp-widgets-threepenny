30b9a4c4a8ecc86c53fddb4345811971af2af9aa
Peter Duchovni <caufeminecraft@gmail.com>
2017-12-13 13:47:06 -0500

    added export module to avoid conflicts

---
+++ Widgets/Threepenny.hs
@@ -0,0 +1,21 @@
+module Widgets.Threepenny (
+  module Widgets.Threepenny.Cast,
+  module Widgets.Threepenny.Core,
+  module Widgets.Threepenny.Links,
+  module Widgets.Threepenny.MinMax,
+  module Widgets.Threepenny.MonoSelect,
+  module Widgets.Threepenny.MultiSelect,
+  module Widgets.Threepenny.Obscura,
+  module Widgets.Threepenny.Ranger,
+  module Widgets.Threepenny.SearchBar
+  ) where
+
+import qualified Widgets.Cast as Widgets.Threepenny.Cast
+import qualified Widgets.Core as Widgets.Threepenny.Core
+import qualified Widgets.Links as Widgets.Threepenny.Links
+import qualified Widgets.MinMax as Widgets.Threepenny.MinMax
+import qualified Widgets.MonoSelect as Widgets.Threepenny.MonoSelect
+import qualified Widgets.MultiSelect as Widgets.Threepenny.MultiSelect
+import qualified Widgets.Obscura as Widgets.Threepenny.Obscura
+import qualified Widgets.Ranger as Widgets.Threepenny.Ranger
+import qualified Widgets.SearchBar as Widgets.Threepenny.SearchBar
